e9ee758f138d6c7d88c9f29d6da432c19a1f6b4a
Laust <lafa16ac@student.cbs.dk>
2018-11-09 10:00:19 +0100

    fix merge conflict

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -4,14 +4,7 @@ import {createSwitchNavigator} from 'react-navigation';
 import MainTabNavigator from './MainTabNavigator';
 
 export default createSwitchNavigator({
-<<<<<<< HEAD
-    // You could add another route here for authentication.
-    // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    Main: MainTabNavigator,
-=======
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Main: MainTabNavigator,
-
->>>>>>> origin/master
 });
