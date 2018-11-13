faab46ee7570e2964472e7d5d6364ad6314e874b
Laust <lafa16ac@student.cbs.dk>
2018-11-13 19:41:23 +0100

    clean up register screen

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
 import Styles from '../assets/Styles';
-import LoginForm from "./LoginForm";
 
 export default class RegisterScreen extends React.Component {
 
@@ -21,21 +20,7 @@ export default class RegisterScreen extends React.Component {
         <Text>
           Register Screen
         </Text>
-        {this.renderButton()}
       </View>
     );
   }
-
-  renderButton() {
-    return (
-      <Button title={'Back'} style={Styles.buttonStyle} onPress={RegisterScreen.sendToLogin.bind(this)}/>
-    );
-  }
-
-  static sendToLogin() {
-    return (
-      <LoginForm/>
-    )
-  }
-
 }
