f567b87a74edb14933b7615d535915d56b0dc1f5
Mathias Tack Olsen <Maol16ac@student.cbs.dk>
2018-11-15 14:36:12 +0100

    register works

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,26 +1,127 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, TextInput, StyleSheet, View, ActivityIndicator, Button} from 'react-native';
 import Styles from '../assets/Styles';
+import firebase from 'firebase';
 
 export default class RegisterScreen extends React.Component {
 
   constructor(props) {
     super(props);
 
-    this.state = {}
+    this.state = {
+      email: '',
+      password: '',
+      loading: false
+    }
   }
 
-  static navigationOptions = {
-    title: 'Please register',
+  onButtonPress() {
+    const {email, password} = this.state;
+
+    this.setState({
+      error: '',
+      loading: true
+    });
+
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(this.onSignUpSucces.bind(this))
+      .catch(this.onSignUpFailed.bind(this));
+
+  }
+
+  onSignUpSucces() {
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: ''
+    });
+    alert("Bruger er nu oprettet")
+    this.setProfile();
+  }
+
+  onSignUpFailed(err) {
+    this.setState({
+      loading: false,
+      error: err.message
+    });
+
+  }
+
+  setProfile = () => {
+    const ref = firebase.database().ref(`/Brugere/${firebase.auth().currentUser.uid}`);
+    const obj = {
+      name: this.state.name,
+      Billetter: {
+        Inaktive: {
+          ignore: 'ignore'
+        },
+        Aktive: {
+          ignore: 'ignore'
+        }
+      }
+    };
+    ref.set(obj)
   };
+
 
   render() {
     return (
       <View style={Styles.containerStyle}>
-        <Text>
-          Register Screen
+        <TextInput
+          label='Username'
+          placeholder='Email'
+          value={this.state.email}
+          onChangeText={email => this.setState({email})}
+        />
+        <TextInput
+          placeholder='Password'
+          value={this.state.password}
+          secureTextEntry={true}
+          onChangeText={password => this.setState({password})}
+        />
+
+        <TextInput
+          placeholder='Name'
+          value={this.state.name}
+          onChangeText={name => this.setState({name})}
+        />
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
         </Text>
+
+        {this.renderButton()}
+
       </View>
     );
   }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <ActivityIndicator size='small'/>
+    }
+    return (
+      <Button title="Sign up" onPress={this.onButtonPress.bind(this)}>
+      </Button>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+});
+
+
+
+
+
+
+
+
+
+
